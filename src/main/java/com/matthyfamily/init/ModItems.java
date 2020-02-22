fbfd29afa3f8f7ad02fe696b1cf9ef59fb0e24e0
matthyno <34276633+MatthyPlayz@users.noreply.github.com>
2020-02-22 13:29:00 -0500

    1.0

--- src/main/java/com/matthyfamily/init/ModItems.java
+++ src/main/java/com/matthyfamily/init/ModItems.java
@@ -1,10 +1,12 @@
 package com.matthyfamily.init;
 
 import com.matthyfamily.MatthysChedibles;
+import net.minecraft.item.Food;
 import net.minecraft.item.Item;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.registries.ObjectHolder;
 
 @Mod.EventBusSubscriber(modid = MatthysChedibles.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
@@ -12,6 +14,7 @@ import net.minecraftforge.registries.ObjectHolder;
 public class ModItems {
 
     public static final Item sodium_chloride = null;
+    public static Food bacon = null;
 
     /**
      * The actual event handler that registers the custom items.
@@ -22,6 +25,9 @@ public class ModItems {
     public static void registerItems(RegistryEvent.Register<Item> event) {
         //In here you pass in all item instances you want to register.
         //Make sure you always set the registry name.
+        // The food register looks like: (new Food.Builder()).hunger(int hunger).saturation(saturation (float+F)).build();
+        // Set the food
+        bacon = (new Food.Builder()).hunger(10).saturation(1.0F).build();
         event.getRegistry().registerAll(
                 new Item(new Item.Properties()).setRegistryName(MatthysChedibles.MOD_ID, "sodium_chloride"),
                 new Item(new Item.Properties()).setRegistryName(MatthysChedibles.MOD_ID, "distillery"),
@@ -29,8 +35,12 @@ public class ModItems {
                 new Item(new Item.Properties()).setRegistryName(MatthysChedibles.MOD_ID, "used_distillery_nitrogen_dioxide"),
                 new Item(new Item.Properties()).setRegistryName(MatthysChedibles.MOD_ID, "filter"),
                 new Item(new Item.Properties()).setRegistryName(MatthysChedibles.MOD_ID, "used_filter_aluminium"),
-                new Item(new Item.Properties()).setRegistryName(MatthysChedibles.MOD_ID, "aluminium_dust")
+                new Item(new Item.Properties()).setRegistryName(MatthysChedibles.MOD_ID, "aluminium_dust"),
+                new Item(new Item.Properties()).setRegistryName(MatthysChedibles.MOD_ID, "nitric_acid"),
+                new Item(new Item.Properties()).setRegistryName(MatthysChedibles.MOD_ID, "sodium_nitrate"),
+                new Item(new Item.Properties()).setRegistryName(MatthysChedibles.MOD_ID, "raw_bacon"),
+                new Item(new Item.Properties().food(bacon)).setRegistryName(MatthysChedibles.MOD_ID, "bacon")
+                // new Item(new Item.Properties()).setRegistryName(MatthysChedibles.MOD_ID, "bacon")
         );
     }
-
 }
